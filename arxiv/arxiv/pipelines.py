d0383b2244b700e037ea4fd30eeb135656364ec2
Cheedge <cheedgelee@gmail.com>
2022-05-21 17:01:02 +0200

    arxiv spider

---
+++ arxiv/arxiv/pipelines.py
@@ -0,0 +1,17 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+import json
+
+class ArxivPipeline:
+    def __init__(self) -> None:
+        self.f = open("/home/sharma/Desktop/DeepLearning/Scraper/arxiv/arxiv_python.json", "w")
+    def process_item(self, item, spider):
+        content = json.dumps(dict(item))+"\n"
+        self.f.write(content)
+        return item
